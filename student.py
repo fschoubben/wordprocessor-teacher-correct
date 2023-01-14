6c8deda230d866d094968c62637f82e08b838ccd
mdpschfr <fsc@webevasion.be>
2023-01-14 19:49:34 +0100

    initial release AKA first git test, don't blame me please

---
+++ student.py
@@ -0,0 +1,83 @@
+class Student:
+    # name = ""
+    # firstname = ""
+    # group = "Unknown"
+    # to_check_manually = ""
+    # to_check = set()
+    max_points = {
+        "format": 2,
+        "nomFichiers": 2,
+        "poids": 2,
+        "orthographe": 0,
+        "pages": 2,
+        "styles": 4,
+        "piedDePage": 4,
+        "espaces": 4,
+        "TDM": 4,
+        "listes": 2,
+        "citation": 2,
+        "noteBasPage": 2,
+        "lien": 2,
+        "images": 4,
+        "section": 0
+    }
+    # reasons = {
+    #     "format": "",
+    #     "nomFichiers": "",
+    #     "poids": "",
+    #     "orthographe": "",
+    #     "pages": "",
+    #     "styles": "",
+    #     "piedDePage": "",
+    #     "espaces": "",
+    #     "TDM": "",
+    #     "listes": "",
+    #     "citation": "",
+    #     "noteBasPage": "",
+    #     "lien": "",
+    #     "images": "",
+    #     "section": ""
+    # }
+    # scores = {
+    #     "format": 0,
+    #     "nomFichiers": 0,
+    #     "poids": 0,
+    #     "orthographe": 0,
+    #     "pages": 0,
+    #     "styles": 0,
+    #     "piedDePage": 0,
+    #     "espaces": 0,
+    #     "TDM": 0,
+    #     "listes": 0,
+    #     "citation": 0,
+    #     "noteBasPage": 0,
+    #     "lien": 0,
+    #     "images": 0,
+    #     "section": 0
+    # }
+
+    def reset(self):
+        self.scores = {"format": 0, "nomFichiers": 0, "poids": 0, "orthographe": 0, "pages": 0,
+                       "styles": 0, "piedDePage": 0, "espaces": 0, "TDM": 0, "listes": 0, "citation": 0,
+                       "noteBasPage": 0, "lien": 0, "images": 0, "section": 0}
+        self.reasons = {"format": "", "nomFichiers": "", "poids": "", "orthographe": "", "pages": "",
+                        "styles": "", "piedDePage": "", "espaces": "", "TDM": "", "listes": "", "citation": "",
+                        "noteBasPage": "", "lien": "", "images": "", "section": ""}
+        self.name = ""
+        self.firstname = ""
+        self.group = "Unknown"
+        self.to_check_manually = ""
+        self.to_check = set()
+
+    def __init__(self):
+        self.scores = {"format": 0, "nomFichiers": 0, "poids": 0, "orthographe": 0, "pages": 0,
+                       "styles": 0, "piedDePage": 0, "espaces": 0, "TDM": 0, "listes": 0, "citation": 0,
+                       "noteBasPage": 0, "lien": 0, "images": 0, "section": 0}
+        self.reasons = {"format": "", "nomFichiers": "", "poids": "", "orthographe": "", "pages": "",
+                        "styles": "", "piedDePage": "", "espaces": "", "TDM": "", "listes": "", "citation": "",
+                        "noteBasPage": "", "lien": "", "images": "", "section": ""}
+        self.name = ""
+        self.firstname = ""
+        self.group = "Unknown"
+        self.to_check_manually = ""
+        self.to_check = set()
